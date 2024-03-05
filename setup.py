6a1509c10b420ccad17f9736ce0483f9b7971c81
InnoftWowq0 <shixiao0814@gmail.com>
2024-03-05 14:28:49 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='my_calculator',
+    version='1.0',
+    description='A simple calculator package',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=['my_calculator'],
+)
